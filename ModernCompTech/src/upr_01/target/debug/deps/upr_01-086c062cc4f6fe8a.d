5d50f8c331b5453b8fd1ca3c411a50031bb268e6
Trofim O. <trofimovchiinikov@gmail.com>
2025-09-08 18:58:21 +0500

    upr_01

---
+++ ModernCompTech/src/upr_01/target/debug/deps/upr_01-086c062cc4f6fe8a.d
@@ -0,0 +1,5 @@
+/home/trofim/Projects/MasterDegree/ModernCompTech/src/upr_01/target/debug/deps/upr_01-086c062cc4f6fe8a.d: src/main.rs
+
+/home/trofim/Projects/MasterDegree/ModernCompTech/src/upr_01/target/debug/deps/upr_01-086c062cc4f6fe8a: src/main.rs
+
+src/main.rs:
---
+++ ModernCompTech/src/upr_01/target/debug/upr_01.d
@@ -0,0 +1,1 @@
+/home/trofim/Projects/MasterDegree/ModernCompTech/src/upr_01/target/debug/upr_01: /home/trofim/Projects/MasterDegree/ModernCompTech/src/upr_01/src/main.rs
